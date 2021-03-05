8bb430b4fe21bd438e7f0b8812b90602617361ce
he-leon <he-leon@users.noreply.github.com>
2021-03-05 17:54:28 +0100

    Update main.ino

--- sender/src/main.ino
+++ sender/src/main.ino
@@ -1,6 +1,6 @@
 /*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
+  Adapted from Rui Santos
+  see https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
 *********/
 
 //Libraries for LoRa
